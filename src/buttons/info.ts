147bcf82eb480266aa5d36b3f24a87d57febce73
Hazim Arafa <hazimarafa80@gmail.com>
2022-06-27 01:18:38 -0700

    Update info.ts

--- src/buttons/info.ts
+++ src/buttons/info.ts
@@ -1,5 +1,8 @@
 import { ButtonInteraction, Message } from "discord.js";
 
 export default function (interaction: ButtonInteraction) {
+  // check if user has voted in the last 24 hours
+  
+
   (interaction.message as Message).delete();
 }
