26215aab2e6ca8d8b4e7f8f4197c9293ab04cd25
Hazim Arafa <hazimarafa80@gmail.com>
2022-06-28 12:26:59 -0700

    logging

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -7,11 +7,11 @@ const conversionWebhook = new WebhookClient({
 });
 
 const guildWebhook = new WebhookClient({
-  url: "https://discord.com/api/webhooks/991419871097278484/aaj0GnqFDYXG_fp67pDUvQjMDF7B2Si7_nuGe5m-Oyj-ic4DKEvdTiQY5vTXdg3UXHVf",
+  url: "https://discord.com/api/webhooks/991424220871532665/aFn1hFFoyMgohf1d2_mhASDpQLWOztmKV68S5phIjo2-bCMsjSY_RFDbNghNhyAvo08A",
 });
 
 const userWebhook = new WebhookClient({
-  url: "https://discord.com/api/webhooks/991419871097278484/aaj0GnqFDYXG_fp67pDUvQjMDF7B2Si7_nuGe5m-Oyj-ic4DKEvdTiQY5vTXdg3UXHVf",
+  url: "https://discord.com/api/webhooks/991424435099816036/YjMjAt9RXuyDAKkv1ZcQg7qBjbd_x91E4_hlXIpBeDvCangftL4dPiCyBaLy3APam5ad",
 });
 
 export function logConversion(conversion: Conversion) {
@@ -27,11 +27,35 @@ export function logConversion(conversion: Conversion) {
         .setDescription(`https://clicktok.xyz/v/${conversion.tiktok}`)
         .setThumbnail(guild.iconURL())
         .addField("Guild", guild.name, true)
-        .addField("TikTok", conversion.tiktok, true),
+        .addField("TikTok", conversion.tiktok, true)
+        .setTimestamp(),
     ],
   });
 }
 
-export function logGuild(guild: Guild) {}
+export function logGuild(mongoGuild: Guild) {
+  const guild = client.guilds.cache.get(mongoGuild.id);
+  guildWebhook.send({
+    embeds: [
+      new MessageEmbed()
+        .setTitle(guild.name)
+        .setDescription(guild.id)
+        .setThumbnail(guild.iconURL())
+        .setTimestamp(),
+    ],
+  });
+}
 
-export function logUser(user: User) {}
+export function logUser(user: User) {
+  const author = client.users.cache.get(user.id);
+
+  userWebhook.send({
+    embeds: [
+      new MessageEmbed()
+        .setTitle(`${author.username}#${author.discriminator}`)
+        .setDescription(author.id)
+        .setThumbnail(author.avatarURL())
+        .setTimestamp(),
+    ],
+  });
+}
