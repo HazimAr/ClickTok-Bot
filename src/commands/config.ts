2a1c6aae12c4ed818fc12cfce11ea97c8a236fb2
Hazim Arafa <hazimarafa80@gmail.com>
2022-06-28 14:46:34 -0700

    Update config.ts

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -37,7 +37,11 @@ export default {
     ),
   run: async function run(interaction: CommandInteraction) {
     const member = interaction.member as GuildMember;
-    if (!member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return interaction.reply({content:"You do not have permission to run this command",ephemeral:true,});
+    if (!member.permissions.has(Permissions.FLAGS.ADMINISTRATOR))
+      return interaction.reply({
+        content: "You do not have permission to run this command",
+        ephemeral: true,
+      });
     const mongoGuild = await getOrCreateGuild(interaction.guild);
 
     const settings = mongoGuild.settings;
