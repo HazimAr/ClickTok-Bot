1f57f42ba815216b5b7d2522217bfcccbeacb23f
Hazim Arafa <hazimarafa80@gmail.com>
2022-06-27 21:38:32 -0700

    Update info.ts

--- src/buttons/info.ts
+++ src/buttons/info.ts
@@ -8,33 +8,33 @@ import {
 import { getOrCreateUser } from "../utils/db";
 
 export default async function (interaction: ButtonInteraction) {
-  // check if user has voted in the last 24 hours
-  const mongoUser = await getOrCreateUser(interaction.user);
+  // // check if user has voted in the last 24 hours
+  // const mongoUser = await getOrCreateUser(interaction.user);
 
-  // check if voted in the last 24 hours
-  if (!(mongoUser.lastVotedAt.getTime() > Date.now() - 86400000))
-    return await interaction.reply({
-      embeds: [
-        new MessageEmbed()
-          .setTitle(`Hey, ${interaction.user.username}`)
-          .setDescription(
-            "It looks like you haven't voted in the last 24 hours. To help keep this bot free votes are needed. Once you have voted you can view your info."
-          ),
-      ],
-      components: [
-        new MessageActionRow().addComponents(
-          new MessageButton()
-            .setLabel("Vote")
-            .setURL(`https://top.gg/bot/${interaction.client.user.id}/vote`)
-            .setStyle("LINK"),
-          new MessageButton()
-            .setCustomId("info")
-            .setLabel("I voted (give me my info)")
-            .setStyle("PRIMARY")
-            .setEmoji("🖥️")
-        ),
-      ],
-    });
+  // // check if voted in the last 24 hours
+  // if (!(mongoUser.lastVotedAt.getTime() > Date.now() - 86400000))
+  //   return await interaction.reply({
+  //     embeds: [
+  //       new MessageEmbed()
+  //         .setTitle(`Hey, ${interaction.user.username}`)
+  //         .setDescription(
+  //           "It looks like you haven't voted in the last 24 hours. To help keep this bot free votes are needed. Once you have voted you can view your info."
+  //         ),
+  //     ],
+  //     components: [
+  //       new MessageActionRow().addComponents(
+  //         new MessageButton()
+  //           .setLabel("Vote")
+  //           .setURL(`https://top.gg/bot/${interaction.client.user.id}/vote`)
+  //           .setStyle("LINK"),
+  //         new MessageButton()
+  //           .setCustomId("info")
+  //           .setLabel("I voted (give me my info)")
+  //           .setStyle("PRIMARY")
+  //           .setEmoji("🖥️")
+  //       ),
+  //     ],
+  //   });
   const { data: tiktok } = await axios.get(
     `https://api2.musical.ly/aweme/v1/aweme/detail/?aweme_id=${
       interaction.message.content.split("/")[
@@ -57,7 +57,7 @@ export default async function (interaction: ButtonInteraction) {
           iconURL: author.avatar_thumb.url_list[0],
         })
         .setTitle(description)
-        .setThumbnail("https://hazim.tech/logo.png")
+        .setThumbnail(tiktok.aweme_detail.video.cover.url_list[0])
         .setDescription(
           `https://clicktok.xyz/v/${tiktok.aweme_detail.aweme_id}`
         )
