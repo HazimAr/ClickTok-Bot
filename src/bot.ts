3edcb553c939fe46c7d25ce325ba5d979beb0f81
Hazim Arafa <hazimarafa80@gmail.com>
2022-06-29 13:42:41 -0700

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -23,26 +23,7 @@ export const client = new Client({
   ],
 });
 
-export const prisma = new PrismaClient({
-  log: [
-    {
-      emit: "stdout",
-      level: "query",
-    },
-    {
-      emit: "stdout",
-      level: "error",
-    },
-    {
-      emit: "stdout",
-      level: "info",
-    },
-    {
-      emit: "stdout",
-      level: "warn",
-    },
-  ],
-});
+export const prisma = new PrismaClient();
 prisma
   .$connect()
   .then(() => {
