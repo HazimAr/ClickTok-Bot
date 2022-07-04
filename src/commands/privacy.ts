1ebedf1e9bffb92b8373ee8e2dbb1631be4758fe
ChickPS5 <108319483+ChickParam@users.noreply.github.com>
2022-07-04 16:33:25 -0400

    privacy and term command add

---
+++ src/commands/privacy.ts
@@ -0,0 +1,15 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("privacy")
+    .setDescription("Sends the link to the Privacy Policy page on the website."),
+
+  run: async function run(interaction: CommandInteraction) {
+    await interaction.reply({
+      content: "https://clicktok.xyz/privacypolicy.pdf",
+      ephemeral: true,
+    });
+  },
+};
---
+++ src/commands/terms.ts
@@ -0,0 +1,15 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("terms")
+    .setDescription("Sends the link to the Terms of Use page on the website."),
+
+  run: async function run(interaction: CommandInteraction) {
+    await interaction.reply({
+      content: "https://clicktok.xyz/terms.pdf",
+      ephemeral: true,
+    });
+  },
+};
