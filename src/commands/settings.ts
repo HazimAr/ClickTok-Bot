e9cdc3fcbd1a9b1d3d4eee5a07ea45be98e8a9ec
Hazim <hazimarafa80@gmail.com>
2022-07-12 20:48:23 -0400

    Update settings.ts

--- src/commands/settings.ts
+++ src/commands/settings.ts
@@ -33,21 +33,21 @@ export default {
         .setDescription(
           "Remove the original embed discord gives when a TikTok link is found in a message. (Default: true)"
         )
-    )
-    .addBooleanOption((option) =>
-      option
-        .setName("public")
-        .setDescription(
-          "Allow your server leaderboard to be seen by anyone who has the link. (Default: true)"
-        )
     ),
+    // .addBooleanOption((option) =>
+    //   option
+    //     .setName("public")
+    //     .setDescription(
+    //       "Allow your server leaderboard to be seen by anyone who has the link. (Default: true)"
+    //     )
+    // ),
   run: async function run(interaction: CommandInteraction) {
     await interaction.reply({
       embeds: [
         new MessageEmbed()
           .setTitle("We Moved Settings To The Dashboard")
           .setDescription(
-            "Please visit the dashboard to configure the bot [here](https://clicktok.xyz/dashboard)."
+            `Please visit the dashboard to configure the bot [here](https://clicktok.xyz/dashboard/${interaction.guild.id}).`
           )
           .setURL("https://clicktok.xyz/dashboard")
           .setColor("#00ff00"),
