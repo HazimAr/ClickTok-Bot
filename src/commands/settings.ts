45e505d8526df49f4c5d7f02d9ceb1e35283e50a
Hazim Arafa <hazimarafa80@gmail.com>
2022-07-19 12:32:36 -0700

    Update settings.ts

--- src/commands/settings.ts
+++ src/commands/settings.ts
@@ -12,35 +12,7 @@ import { getOrCreateGuild } from "../utils/db";
 export default {
   data: new SlashCommandBuilder()
     .setName("settings")
-    .setDescription("Configure the bot for your server.")
-    .addBooleanOption((option) =>
-      option
-        .setName("auto_embed")
-        .setDescription(
-          "Automatically embed TikToks in the message if they are found. (Default: true)"
-        )
-    )
-    .addBooleanOption((option) =>
-      option
-        .setName("delete_origin")
-        .setDescription(
-          "Delete the original message if a TikTok is found in it after sending the embed. (Default: false)"
-        )
-    )
-    .addBooleanOption((option) =>
-      option
-        .setName("suppress_embed")
-        .setDescription(
-          "Remove the original embed discord gives when a TikTok link is found in a message. (Default: true)"
-        )
-    ),
-    // .addBooleanOption((option) =>
-    //   option
-    //     .setName("public")
-    //     .setDescription(
-    //       "Allow your server leaderboard to be seen by anyone who has the link. (Default: true)"
-    //     )
-    // ),
+    .setDescription("Configure the bot for your server."),
   run: async function run(interaction: CommandInteraction) {
     await interaction.reply({
       embeds: [
